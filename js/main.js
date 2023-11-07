305b1701eae5d0ef39d3e7346110b2dcefba4059
Rustamov-Javohir <javohirrustamov603@gmail.com>
2023-11-07 11:16:47 +0500

    Portfolio done

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
 // Main Js
+
+var typingEffect = new Typed(".multiText", {
+    strings: ["coder", "youtuber", "designer"],
+    loop: true,
+    typeSpeed: 100,
+    backSpeed: 80,
+    backDelay: 1500,
+});
